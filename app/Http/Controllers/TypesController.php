bd129abad91f177e470a3fdcf1bd1e4a3dc13436
Shajedul Islam Shuvo <helloitsshuvo@gmail.com>
2019-02-10 23:59:03 +0600

    Units and Product Type can be added from addproduct form now

--- app/Http/Controllers/TypesController.php
+++ app/Http/Controllers/TypesController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\productType;
 use Illuminate\Http\Request;
 
 class TypesController extends Controller
 {
-    //
+    public function store()
+    {
+        $type = new productType();
+        $type->name =\request('new_type');
+        $type->save();
+        return redirect('products/create');
+    }
 }
--- app/Http/Controllers/UnitsController.php
+++ app/Http/Controllers/UnitsController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Unit;
 use Illuminate\Http\Request;
 
 class UnitsController extends Controller
 {
-    //
+    public function store()
+    {
+        $unit = new Unit();
+        $unit->name =\request('new_unit');
+        $unit->save();
+        return redirect('products/create');
+
+    }
 }
