940bbd1f86d73d0a7cb48006ec7ad208a0a5d127
Shajedul Islam Shuvo <helloitsshuvo@gmail.com>
2019-02-13 15:25:05 +0600

    Description of products table is now nullable

--- database/migrations/2019_02_06_075624_create_products_table.php
+++ database/migrations/2019_02_06_075624_create_products_table.php
@@ -20,7 +20,7 @@ class CreateProductsTable extends Migration
             $table->integer('unit_id');
             $table->float('price');
             $table->float('quantity');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->integer('supplier_id');
             $table->timestamps();
         });
